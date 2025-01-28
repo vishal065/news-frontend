b73a2d284a66c9c44f8b07b73efe49f3a3b7b3fb
topDevelopers123 <jhahimanshu498@gmail.com>
2025-01-28 13:42:12 +0530

    header and footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-white-500 bg-red-400'>Hello world</h1>
+      <Header />
+      <Footer />
     </>
   )
 }
