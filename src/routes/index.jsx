4f43afae55e92d6a23754551299ef8836ff32dda
topDevelopers123 <jhahimanshu498@gmail.com>
2025-02-04 10:43:03 +0530

    color changes header

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "./authRoutes/AuthRoutes";
 import UserLayout from "./userRoutes/UserLayoute";
 import NotFound from "../components/NotFound";
+import { UserPublicRoutes } from "./userRoutes/PublicRoutes";
 
 
 const RootRouting = () => {
@@ -11,6 +12,11 @@ const RootRouting = () => {
             <Route element={<UserLayout />}>
                 {AuthRoutes.map((item, index) => <Route key={index} path={item.path} element={item.element} />)}
             </Route>
+
+            {/* <Route element={<UserLayout />}>
+                {UserPublicRoutes.map((item, index) => <Route key={index} path={item.path} element={item.element} />)}
+            </Route> */}
+
 
 
             <Route path="*" element={<NotFound />} />
