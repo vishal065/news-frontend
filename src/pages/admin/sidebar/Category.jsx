b7177cc7ddde44cbb468d715112adad7b3b35483
topDevelopers123 <jhahimanshu498@gmail.com>
2025-02-21 18:52:17 +0530

    handle crud of update and delete in category

--- src/pages/admin/sidebar/Category.jsx
+++ src/pages/admin/sidebar/Category.jsx
@@ -1,31 +1,29 @@
 import React, { useState } from 'react';
 import { useFormik } from 'formik';
-import { useCreateCategory } from '../../../hooks/admin/useCategory';
-import { createCategoryState } from '../../../validation/adminState';
+import { useCreateAndUpdateCategory } from '../../../hooks/admin/useCategory';
 import { createCategorySchema } from '../../../validation/adminValidation';
 import { useQueryCategory } from '../../../hooks/useAdminQuery';
 
 
 const Category = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const { mutate, isPending } = useCreateCategory();
+    const [toggleModal, setToggleModal] = useState({ path: null, state: false });
+    const [oldData, setOldData] = useState(null)
+    const { mutate, isPending } = useCreateAndUpdateCategory();
     const { data } = useQueryCategory();
-    console.log(data)
 
     const { values, errors, touched, handleChange, handleBlur, handleSubmit, resetForm } = useFormik({
-        initialValues: createCategoryState,
+        initialValues: {
+            name: toggleModal?.path === "create" ? "" : oldData?.name,
+        },
         validationSchema: createCategorySchema,
+        enableReinitialize: true,
         onSubmit: (value) => {
-            mutate(value)
+            mutate({ path: toggleModal.path, id: oldData?._id, ...value })
             resetForm();
-            toggleModal();
+            setToggleModal({ path: null, state: false })
         },
     });
 
-    const toggleModal = () => {
-        setIsModalOpen(!isModalOpen);
-        resetForm();
-    };
 
     return (
         <div className="container mx-auto p-4">
@@ -33,7 +31,7 @@ const Category = () => {
             <div className='flex justify-end'>
                 <button
                     className="mb-4 bg-red-600 font-bold text-white px-4 py-2 rounded hover:bg-red-700"
-                    onClick={toggleModal}
+                    onClick={() => setToggleModal((prev) => ({ ...prev, path: "create", state: !prev.state }))}
                 >
                     Add Category
                 </button>
@@ -48,13 +46,16 @@ const Category = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data?.map((item, index) => (
+                        {data && data?.map((item, index) => (
                             <tr key={index} className="border-b border-gray-300 hover:bg-gray-50">
                                 <td className="py-2 px-4">{index + 1}</td>
                                 <td className="py-2 px-4">{item?.name}</td>
                                 <td className="py-2 px-4 text-center">
-                                    <button className="bg-blue-500 text-white px-3 py-1 rounded mr-2 hover:bg-blue-600">Edit</button>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">Delete</button>
+                                    <button onClick={() => {
+                                        setToggleModal((prev) => ({ ...prev, path: "update", state: !prev.state }))
+                                        setOldData(item)
+                                    }} className="bg-blue-500 text-white px-3 py-1 rounded mr-2 hover:bg-blue-600">Edit</button>
+                                    <button onClick={() => mutate({ id: item?._id })} className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">Delete</button>
                                 </td>
                             </tr>
                         ))}
@@ -62,10 +63,11 @@ const Category = () => {
                 </table>
             </div>
 
-            {isModalOpen && (
+            {toggleModal?.state && (
                 <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
                     <div className="bg-white p-5 rounded shadow-lg w-96">
-                        <h3 className="text-lg font-bold mb-3">Add New Category</h3>
+                        <h3 className="text-lg font-bold mb-3">{!toggleModal?.id ? "Add New" : "Update "} Category</h3>
+
                         <form onSubmit={handleSubmit}>
                             <input
                                 type="text"
@@ -76,14 +78,15 @@ const Category = () => {
                                 className="w-full border p-2 mb-2"
                                 placeholder="Enter category name"
                             />
-                            {errors.name && touched.name && (
+                            {errors?.name && touched?.name && (
                                 <p className="text-red-500 text-sm mb-2">{errors.name}</p>
                             )}
                             <div className="flex justify-end">
                                 <button
                                     type="submit"
                                     className="bg-gray-400 text-white px-4 py-2 rounded mr-2 hover:bg-gray-500"
-                                    onClick={toggleModal}
+                                    onClick={() => setToggleModal((prev) => ({ ...prev, path: null, state: !prev.state }))}
+
                                 >
                                     Cancel
                                 </button>
