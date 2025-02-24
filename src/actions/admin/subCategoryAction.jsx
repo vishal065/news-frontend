ebe4e515d2222af5d5ebdf9fcb548cf872621024
vishal065 <dasvishal60@gmail.com>
2025-02-24 12:56:10 +0530

    create news crud and subcategory crud api only

---
+++ src/actions/admin/subCategoryAction.jsx
@@ -0,0 +1,53 @@
+import AxiosHandler from "../AxiosHandler";
+
+
+const axiosInstance = AxiosHandler();
+
+// Create Sub Category 
+const createSubCategory = async (data) => {
+    try {
+        const res = await axiosInstance.post("/subCategory/create", data);
+        return res;
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+// Fetched Sub Category 
+const getSubCategory = async () => {
+    try {
+        const res = await axiosInstance.get("/subCategory/get");
+        return res;
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+// Update Sub Category 
+const updateSubCategory = async (data, id) => {
+    try {
+        const res = await axiosInstance.put(`subCategory/update/${id}`, data);
+        return res;
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+// Delete Sub Category 
+const deleteCategory = async (id) => {
+    try {
+        return await axiosInstance.delete(`subCategory/delete/${id}`);
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+export { createSubCategory, getSubCategory, updateSubCategory, deleteCategory };
--- src/validation/adminValidation.js
+++ src/validation/adminValidation.js
@@ -1,19 +1,42 @@
 import * as yup from "yup";
 
 
-// Create CAtegory 
+// Category 
 export const createCategorySchema = yup.object({
     name: yup.string().trim().required("name is required")
 });
 
+// Sub Category
+export const createSubCategorySchema = yup.object({
+    name: yup.string().trim().required("name is required"),
+    categoryId: yup.string().trim().required("categoryId is required")
+});
 
-// Create Anchor 
+
+//  Anchor 
 export const createAnchorSchema = yup.object({
     name: yup.string().trim().required("name is required")
 });
 
 
-// create Publisher
+//  Publisher
 export const createPublisherSchema = yup.object({
     name: yup.string().trim().required("name is required")
+});
+
+
+//  News
+export const createNewsTableSchema = yup.object({
+    title: yup.string().trim().required("title is required"),
+    slug: yup.string().trim().required("slug is required"),
+    description: yup.string().trim().required("description is required"),
+    metaDesciption: yup.string().trim().required("meta desciption is required"),
+    alt: yup.string().trim().required("alt is required"),
+    type: yup.string().trim().required("type is required"),
+    status: yup.boolean().required("status is required"),
+    videoURL: yup.string().trim().required("videoURL is required"),
+    categoryId: yup.string().trim().required("categoryId is required"),
+    subCategoryId: yup.string().trim().required("subCategoryId is required"),
+    publisherId: yup.string().trim().required("publisherId is required"),
+    anchorId: yup.string().trim().required("anchorId is required")
 });
